e4005b686049835e88f0afb7158ca631e8dbde1f
Marcelo G <m2biel000@icloud.com>
2023-07-31 12:14:03 -0300

    MovieTagsController ok

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,12 @@ const { Router } = require("express");
 
 const usersRoutes = require("./users.routes");
 const movieNotes = require("./movie_notes.routes");
-// const movieTags
+const movieTags = require("./movie_tags.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRoutes);
 routes.use("/movie_notes", movieNotes);
+routes.use("/movie_tags", movieTags);
 
 module.exports = routes;
